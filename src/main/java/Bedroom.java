ef39c32274e5d439747a4c749e9d01bbdeb67262
Anita Safar <ancsa0416@gmail.com>
2022-01-07 17:10:02 +0000

    setRoomNumber method added to bedroom

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -12,4 +12,8 @@ public class Bedroom extends Room {
     public int getRoomNumber() {
         return this.roomNumber;
     }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
 }
