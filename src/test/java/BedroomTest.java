ff209bed1b5b06a285c38e533f123c4fd714d0cc
Anita Safar <ancsa0416@gmail.com>
2022-01-07 16:34:43 +0000

    Guests>capacity test added to bedroomTest

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -6,12 +6,16 @@ import static org.junit.Assert.assertEquals;
 public class BedroomTest {
 
     Guest guest1;
+    Guest guest2;
+    Guest guest3;
     Bedroom bedroom;
 
     @Before
     public void before() {
 
         guest1 = new Guest("Tom Hanks");
+        guest2 = new Guest("Chuck Noland");
+        guest3 = new Guest("Wilson");
         bedroom = new Bedroom(2,416, RoomType.DOUBLE);
     }
 
@@ -26,4 +30,11 @@ public class BedroomTest {
         assertEquals(1, bedroom.guestCount());
     }
 
+    @Test
+    public void moreGuestsThanCapacity() {
+        bedroom.addGuest(guest1);
+        bedroom.addGuest(guest2);
+        bedroom.addGuest(guest3);
+        assertEquals(2, bedroom.guestCount());
+    }
 }
