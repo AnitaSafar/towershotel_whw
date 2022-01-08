c7585e35231af7303bdbd24e09ee9be5f9ab7cda
Anita Safar <ancsa0416@gmail.com>
2022-01-08 14:31:52 +0000

    canRemoveGuestFromBedroom test added to HotelTest

--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -8,11 +8,14 @@ import static org.junit.Assert.assertEquals;
 public class HotelTest {
 
     private Room room1;
+    private Room room2;
 
     private Bedroom bedroom1;
     private Bedroom bedroom2;
 
     private Guest guest1;
+    private Guest guest2;
+    private Guest guest3;
 
     private ConferenceRoom conferenceRoom1;
 
@@ -24,6 +27,8 @@ public class HotelTest {
     bedroom2 = new Bedroom(3, 112, RoomType.TRIPLE);
 
     guest1 = new Guest("Wilson");
+    guest2 = new Guest("Nike");
+    guest3 = new Guest("Spalding");
 
     conferenceRoom1 = new ConferenceRoom(5, "", "Interviews");
 
@@ -41,5 +46,15 @@ public class HotelTest {
         room1 = new Bedroom(1, 202, RoomType.SINGLE);
         room1.addGuest(guest1);
         assertEquals(1, room1.guestCount());
+    }
+
+    @Test
+    public void canRemoveGuestFromBedroom() {
+        room2 = new Bedroom(3, 112, RoomType.TRIPLE);
+        room2.addGuest(guest1);
+        room2.addGuest(guest2);
+        room2.addGuest(guest3);
+        room2.removeGuest(guest2);
+        assertEquals(2, room2.guestCount());
     }
 }
