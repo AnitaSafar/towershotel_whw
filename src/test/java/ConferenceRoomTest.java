9a0c233b07946bed69e4daa0ff18f6c3ec63b2e1
Anita Safar <ancsa0416@gmail.com>
2022-01-08 11:58:29 +0000

    conferenceRoomTest setup done, hasCapacity test added

--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,13 +1,20 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceRoomTest {
 
-//    ConferenceRoom conferenceRoom;
-//
-//    @Before
-//    public void before() {
-//        conferenceRoom = new ConferenceRoom(5, "Green", "Meeting");
-//    }
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before() {
+        conferenceRoom = new ConferenceRoom(5, "Green", "Meeting");
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(5, conferenceRoom.getCapacity());
+    }
 
 }
